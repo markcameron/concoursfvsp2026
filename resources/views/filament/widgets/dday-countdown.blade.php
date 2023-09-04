fd3374b02ae80a7d6237aa76b191317de557a254
Mark Cameron <mark.cameron@devfactory.ch>
2023-09-04 22:38:06 +0200

    Fix typo in widget text

--- resources/views/filament/widgets/dday-countdown.blade.php
+++ resources/views/filament/widgets/dday-countdown.blade.php
@@ -1,7 +1,7 @@
 <x-filament::widget>
     <x-filament::card>
         <h2 class="text-lg font-bold tracking-tight sm:text-xl">
-            Le councours commence {{ \Carbon\Carbon::parse('2026-05-08')->diffForHumans(['parts' => 4]) }}
+            Le concours commence {{ \Carbon\Carbon::parse('2026-05-08')->diffForHumans(['parts' => 4]) }}
         </h2>
     </x-filament::card>
 </x-filament::widget>
