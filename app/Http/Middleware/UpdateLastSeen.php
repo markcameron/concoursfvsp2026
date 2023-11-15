09bed627b0919b7dbf6aa6f14eea0d3977c376f5
Mark Cameron <mark.cameron@devfactory.ch>
2023-11-15 13:55:05 +0100

    Add Last Seen to users

---
+++ app/Http/Middleware/UpdateLastSeen.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UpdateLastSeen
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // dd(auth('web')->check());
+        if (auth('web')->check()) {
+            auth('web')->user()->update(['last_seen' => now()]);
+        }
+
+        return $next($request);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,17 +3,20 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Filament\Panel;
 use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\Hash;
 use Filament\Models\Contracts\HasName;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Althinect\FilamentSpatieRolesPermissions\Concerns\HasSuperAdmin;
 
 class User extends Authenticatable implements FilamentUser, HasName
 {
@@ -21,6 +24,8 @@ class User extends Authenticatable implements FilamentUser, HasName
     use HasFactory;
     use Notifiable;
     use SoftDeletes;
+    use HasRoles;
+    use HasSuperAdmin;
 
     /**
      * The attributes that are mass assignable.
@@ -28,6 +33,7 @@ class User extends Authenticatable implements FilamentUser, HasName
      * @var array<int, string>
      */
     protected $fillable = [
+        'last_seen',
         'first_name',
         'last_name',
         'alias',
