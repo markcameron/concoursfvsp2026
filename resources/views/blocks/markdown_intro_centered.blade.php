f56b089663df60f75f55232ae7c8aac1c8d33af4
Mark Cameron <mark.cameron@devfactory.ch>
2026-02-24 16:47:37 +0100

    style(markdown_intro_centered.blade.php): add flex and gap-y-2 classes for improved layout spacing

--- resources/views/blocks/markdown_intro_centered.blade.php
+++ resources/views/blocks/markdown_intro_centered.blade.php
@@ -1,5 +1,5 @@
 <div class="lg:max-w-(--breakpoint-xs) section-spacing container mx-auto px-4">
-    <div class="text-center text-lg font-medium text-gray-600">
+    <div class="text-center text-lg font-medium text-gray-600 flex flex-col gap-y-2">
         {!! \Str::of($block->body)->markdown() !!}
     </div>
 </div>
