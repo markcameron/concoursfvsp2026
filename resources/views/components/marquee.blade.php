0cb78d12b0e2db63fe2ff7b8dfb5103885ee6104
Mark Cameron <mark.cameron@devfactory.ch>
2026-02-24 15:29:27 +0100

    fix(marquee.blade.php): update sponsor image size from logo_small to logo_large for better visibility

--- resources/views/components/marquee.blade.php
+++ resources/views/components/marquee.blade.php
@@ -4,14 +4,14 @@
             <div class="marquee__group">
                 @foreach ($sponsors as $sponsor)
                     <a href="{{ $sponsor->url }}" target="_blank">
-                        <img src="{{ $sponsor->getImageUrl('logo', 'logo_small') }}" alt="{{ $sponsor->name }} logo" class="h-16">
+                        <img src="{{ $sponsor->getImageUrl('logo', 'logo_large') }}" alt="{{ $sponsor->name }} logo" class="h-16">
                     </a>
                 @endforeach
             </div>
             <div aria-hidden="true" class="marquee__group">
                 @foreach ($sponsors as $sponsor)
                     <a href="{{ $sponsor->url }}" target="_blank">
-                        <img src="{{ $sponsor->getImageUrl('logo', 'logo_small') }}" alt="{{ $sponsor->name }} logo" class="h-16">
+                        <img src="{{ $sponsor->getImageUrl('logo', 'logo_large') }}" alt="{{ $sponsor->name }} logo" class="h-16">
                     </a>
                 @endforeach
             </div>
