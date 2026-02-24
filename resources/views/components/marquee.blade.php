b36b62d76c1c28c48d06b046481f651bcf6ec701
Mark Cameron <mark.cameron@devfactory.ch>
2026-02-24 23:17:41 +0100

    style(marquee.blade.php): adjust sponsor logo height for responsive design

--- resources/views/components/marquee.blade.php
+++ resources/views/components/marquee.blade.php
@@ -4,14 +4,14 @@
             <div class="marquee__group">
                 @foreach ($sponsors as $sponsor)
                     <a href="{{ $sponsor->url }}" target="_blank">
-                        <img src="{{ $sponsor->getImageUrl('logo', 'logo_large') }}" alt="{{ $sponsor->name }} logo" class="h-16">
+                        <img src="{{ $sponsor->getImageUrl('logo', 'logo_large') }}" alt="{{ $sponsor->name }} logo" class="h-8 sm:h-12 md:h-16">
                     </a>
                 @endforeach
             </div>
             <div aria-hidden="true" class="marquee__group">
                 @foreach ($sponsors as $sponsor)
                     <a href="{{ $sponsor->url }}" target="_blank">
-                        <img src="{{ $sponsor->getImageUrl('logo', 'logo_large') }}" alt="{{ $sponsor->name }} logo" class="h-16">
+                        <img src="{{ $sponsor->getImageUrl('logo', 'logo_large') }}" alt="{{ $sponsor->name }} logo" class="h-8 sm:h-12 md:h-16">
                     </a>
                 @endforeach
             </div>
