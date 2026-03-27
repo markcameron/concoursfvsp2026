a47a34aace55b103fe0e8cc338fb016fb6f03a10
Mark Cameron <mark.cameron@devfactory.ch>
2026-03-27 07:59:38 +0100

    refactor(welcome.blade.php): remove sponsor levels section to streamline the view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,29 +71,6 @@
 
     <x-programme />
 
-    @if ($sponsorLevels)
-        <section class="section-spacing container mx-auto px-4">
-            <h2 class="section-title mb-12 text-center">Devenez Sponsor</h2>
-            <div class="grid items-start gap-8 lg:grid-cols-4">
-                @foreach ($sponsorLevels as $level)
-                    <div class="bg-{{ $level->color }}-100 flex w-full flex-col items-center rounded-[32px] p-8 shadow-[0_1px_4px_rgba(0,0,0,0.2)]">
-                        <h3 class="font-display text-theme-{{ $level->color }} text-center text-2xl font-semibold uppercase">{{ $level->name }}</h3>
-                        <img src="{{ asset('images/hose-' . $level->color . '.svg') }}" class="my-8 drop-shadow-[5px_7px_26px_rgba(0,0,0,0.2)]" width="176" height="176" alt="">
-                        <div class="text-theme-{{ $level->color }} text-center text-4xl font-bold uppercase">CHF {{ number_format($level->price, 0, '', '’') }}.-</div>
-                        <ul class="list-checkmark list-checkmark--{{ $level->color }} my-6 space-y-2 font-medium text-gray-600">
-                            @foreach ($level->benefits as $benefit)
-                                <li>{{ $benefit }}</li>
-                            @endforeach
-                        </ul>
-                        <div class="w-full">
-                            <a href="{{ route('sponsor.info') }}" class="btn text-theme-blue w-full bg-white">Sponsoriser</a>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </section>
-    @endif
-
     @if ($showSponsorListHomepage)
         <section class="section-spacing container mx-auto px-4">
             <div class="sponsors-logo-sizing xs:gap-12 mt-3 flex flex-wrap items-center justify-center gap-8 sm:gap-16 lg:gap-20">
