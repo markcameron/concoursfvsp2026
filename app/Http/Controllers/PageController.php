519687c5d9dd625b37e2b18ee98c6725567620f1
Mark Cameron <mark.cameron@devfactory.ch>
2026-04-14 17:36:27 +0200

    feat(seo): integrate SEO package for enhanced metadata management across pages

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Page;
+use RalphJSmit\Laravel\SEO\Support\SEOData;
 
 class PageController extends Controller
 {
@@ -11,7 +12,8 @@ class PageController extends Controller
         $page = Page::where('machine_name', 'committee')->first();
 
         return view('pages.committee')
-            ->with('page', $page);
+            ->with('page', $page)
+            ->with('seoData', new SEOData(title: $page?->title));
     }
 
     public function volunteers()
@@ -19,7 +21,8 @@ class PageController extends Controller
         $page = Page::where('machine_name', 'volunteers')->first();
 
         return view('pages.basic')
-            ->with('page', $page);
+            ->with('page', $page)
+            ->with('seoData', new SEOData(title: $page?->title));
     }
 
     public function station()
@@ -32,7 +35,8 @@ class PageController extends Controller
         $page = Page::where('machine_name', 'donations')->first();
 
         return view('pages.basic')
-            ->with('page', $page);
+            ->with('page', $page)
+            ->with('seoData', new SEOData(title: $page?->title));
     }
 
     public function livret()
@@ -40,7 +44,8 @@ class PageController extends Controller
         $page = Page::where('machine_name', 'livret')->first();
 
         return view('pages.livret')
-            ->with('page', $page);
+            ->with('page', $page)
+            ->with('seoData', new SEOData(title: $page?->title));
     }
 
     public function programme()
@@ -48,7 +53,8 @@ class PageController extends Controller
         $page = Page::where('machine_name', 'program')->first();
 
         return view('pages.programme')
-            ->with('page', $page);
+            ->with('page', $page)
+            ->with('seoData', new SEOData(title: $page?->title));
     }
 
     public function map()
@@ -56,7 +62,8 @@ class PageController extends Controller
         $page = Page::where('machine_name', 'map')->first();
 
         return view('pages.map')
-            ->with('page', $page);
+            ->with('page', $page)
+            ->with('seoData', new SEOData(title: $page?->title));
     }
 
     public function procession()
@@ -64,6 +71,7 @@ class PageController extends Controller
         $page = Page::where('machine_name', 'procession')->first();
 
         return view('pages.procession')
-            ->with('page', $page);
+            ->with('page', $page)
+            ->with('seoData', new SEOData(title: $page?->title));
     }
 }
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>FVSP Terre Sainte 2026</title>
+    {!! isset($seoData) ? seo($seoData) : seo() !!}
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
