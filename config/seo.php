c1b21edf2acdeae129e19863443f3bc3765706a8
Mark Cameron <mark.cameron@devfactory.ch>
2026-04-14 17:45:35 +0200

    fix(seo.php): update fallback image path for social sharing to logo-social.png

--- config/seo.php
+++ config/seo.php
@@ -101,7 +101,7 @@ return [
          * This should be a path to an image. The url to the path is generated using the `secure_url()` function
          * (`secure_url($yourProvidedPath)`), so make sure the image is accessible from the public folder.
          */
-        'fallback' => 'images/logo.png',
+        'fallback' => 'images/logo-social.png',
     ],
 
     'author' => [
