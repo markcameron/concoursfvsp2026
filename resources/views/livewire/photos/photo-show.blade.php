b83ce7db5d362cf5e9245a0c20f1f9d64a2c6430
Mark Cameron <mark.cameron@devfactory.ch>
2026-05-13 19:59:04 +0200

    feat(photo-show): display photo taken date using EXIF data if available

--- resources/views/livewire/photos/photo-show.blade.php
+++ resources/views/livewire/photos/photo-show.blade.php
@@ -65,7 +65,11 @@
                     <svg class="size-4" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
                     </svg>
-                    <span>{{ $photo->created_at->format('d.m.Y') }}</span>
+                    @if (!empty($photo->exif_data['taken_at']))
+                        <span>{{ \Carbon\Carbon::parse($photo->exif_data['taken_at'])->format('d.m.Y') }}</span>
+                    @else
+                        <span>{{ $photo->created_at->format('d.m.Y') }}</span>
+                    @endif
                 </div>
                 <div class="flex items-center gap-1">
                     <svg class="size-4" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24">
@@ -99,9 +103,6 @@
                     @endif
                     @if (!empty($exif['iso']))
                         <div>ISO {{ $exif['iso'] }}</div>
-                    @endif
-                    @if (!empty($exif['taken_at']))
-                        <div>{{ \Carbon\Carbon::parse($exif['taken_at'])->format('d.m.Y H:i') }}</div>
                     @endif
                     @if (!empty($exif['width']) && !empty($exif['height']))
                         <div>{{ $exif['width'] }}×{{ $exif['height'] }}</div>
